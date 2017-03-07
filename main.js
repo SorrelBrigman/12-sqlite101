8241e36579e884b43b4168c2f1e2e744646f3219
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-07 10:56:13 -0600

    using db.all to query db

--- main.js
+++ main.js
@@ -31,4 +31,18 @@ db.run("CREATE TABLE IF NOT EXISTS employees(id INT, first TEXT, last TEXT, sala
   })
 
  };
- populateEmployees();
+ // populateEmployees();
+
+
+//db.get gets just the first row
+ // db.get(`SELECT * FROM employees`, (err, row)=>{
+ //  console.log(row);
+ // })
+
+
+//db.all gets all the rows, and returns them as an array
+ db.all(`SELECT * FROM employees`, (err, allRows)=>{
+  allRows.forEach(({id, first, last, dept, salary})=>{
+    console.log(`Employee ${id} is ${first} ${last}, and they make $ ${salary} working in the ${dept} department.`);
+  })
+ })
